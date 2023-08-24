a2401d3a3e138a15915517c45e4efffeb10faef7
Viktor <novikov.vd@phystech.edu>
2023-08-24 12:23:32 +0300

    magical constants fixed

--- output.cpp
+++ output.cpp
@@ -5,19 +5,19 @@ void output_coefficients (const solutions roots)
 {
     switch(roots.num_of_roots)
     {
-    case 0:
+    case ZERO_ROOTS:
         printf("No roots\n");
         break;
 
-    case 1:
+    case ONE_ROOT:
         printf("One root:%lf\n", roots.x1);
         break;
 
-    case 2:
+    case TWO_ROOTS:
         printf("Two roots:%lf %lf\n", roots.x1, roots.x2);
         break;
 
-    case (-1):
+    case INF_ROOTS:
         printf("Infinity roots\n");
         break;
 
